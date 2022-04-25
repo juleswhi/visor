46ced430308bcc2906f307e1502eafa9b4bc74f0
Juliet <juliet.whitte@gmail.com>
2022-04-25 22:28:20 +0100

    inital changes

---
+++ Assets/basic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basic : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private float xInput, yInput;
+
+    public float mms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mms = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private
+    void FixedUpdate() {
+        Movement();    
+    }
+
+
+    private void GetInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+
+    private void Movement()
+    {
+        rb.AddForce(xInput * mms, 0 , yInput * mms);
+    }
+}
