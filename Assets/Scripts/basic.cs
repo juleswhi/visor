7bb375597db6b21f21de84c242e8e8eca8dd5d07
Juliet <juliet.whitte@gmail.com>
2022-04-26 21:40:28 +0100

    gun changes

---
+++ Assets/Scripts/basic.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basic : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public CharacterController controller;
+    public Transform groundCheck;    
+    private Vector3 velocity;
+    private float xInput, zInput, gravity;
+    public float mms, groundDistance, jumpHeight;
+    public LayerMask groundMask;
+    private bool isGrounded;
+    public Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AssignValues();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xInput + transform.forward * zInput;
+
+        controller.Move(move * mms * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+
+    private void AssignValues()
+    {
+        rb = GetComponent<Rigidbody>();
+        mms = 10f;
+        gravity = -25f;
+        groundDistance = 0.4f;
+        jumpHeight = 6.5f;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("test");
+        gameObject.transform.position = new Vector3(0f, 10f, 0f);
+    }
+
+}
---
+++ Assets/gun.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 30f;
+
+    public ParticleSystem muzzleFlash;
+
+    public Camera fpsCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
+    }
+}
